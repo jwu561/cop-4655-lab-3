a938af821b24e493aa7e782064a03354f4aff5c3
jwu561 <59592585+jwu561@users.noreply.github.com>
2023-09-17 13:50:06 -0400

    Added album icon

--- lab-tunley/AlbumsViewController.swift
+++ lab-tunley/AlbumsViewController.swift
@@ -105,9 +105,41 @@ class AlbumsViewController: UIViewController, UICollectionViewDataSource {
                 print(error.localizedDescription)
             }
 
+            
+            
+            
+            
+            
+            
 
         }
 
+        
+        
+        // Get a reference to the collection view's layout
+        // We want to dynamically size the cells for the available space and desired number of columns.
+        // NOTE: This collection view scrolls vertically, but collection views can alternatively scroll horizontally.
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+
+        // The minimum spacing between adjacent cells (left / right, in vertical scrolling collection)
+        // Set this to taste.
+        layout.minimumInteritemSpacing = 4
+
+        // The minimum spacing between adjacent cells (top / bottom, in vertical scrolling collection)
+        // Set this to taste.
+        layout.minimumLineSpacing = 4
+
+        // Set this to however many columns you want to show in the collection.
+        let numberOfColumns: CGFloat = 3
+
+        // Calculate the width each cell need to be to fit the number of columns, taking into account the spacing between cells.
+        let width = (collectionView.bounds.width - layout.minimumInteritemSpacing * (numberOfColumns - 1)) / numberOfColumns
+
+        // Set the size that each tem/cell should display at
+        layout.itemSize = CGSize(width: width, height: width)
+
+
+        
         // Initiate the network request
         task.resume()
 
